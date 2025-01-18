652dbf380b05b5563a8740fe8713a61515b59640
Virat IT DELL <nadimpathan19802@gmail.com>
2025-01-18 17:44:10 +0530

    host changes

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
