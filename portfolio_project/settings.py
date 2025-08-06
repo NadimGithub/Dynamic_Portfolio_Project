d03140042a84e1e5c89e6dfbd6dec304b30b89a9
Nadim Pathan <146571150+NadimGithub@users.noreply.github.com>
2025-08-06 13:38:41 +0530

    Update settings.py

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -113,9 +113,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ads.vijay24@gmail.com'
-EMAIL_HOST_PASSWORD = 'wdogahkcslzgedxf' # Use App Password generated from Google Account
-DEFAULT_FROM_EMAIL = 'Portfolio Website <ads.vijay24@gmail.com>'
+EMAIL_HOST_USER = 'nadim.dp19@gmail.com'
+EMAIL_HOST_PASSWORD = 'imumdhiddgaqvcxy' # Use App Password generated from Google Account
+DEFAULT_FROM_EMAIL = 'Portfolio Website <nadim.dp19@gmail.com>'
 EMAIL_TIMEOUT = 30  # Timeout in seconds
 
 # Login configuration
